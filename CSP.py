49d982646caf355515019f255044fcc9655c2c45
qasim12343 <qasem.yousifi20@gmail.com>
2024-05-14 00:10:49 +0330

    getPosition, getActions and testCases added

--- CSP.py
+++ CSP.py
@@ -1,12 +1,14 @@
-def valid_row(row, numbers):
+
+
+def valid_Column(column, numbers):
     count = 0
-    for i in range(len(row)):
-        b = row[i]
+    for i in range(len(column)):
+        b = column[i]
         # print(b)
         if b == '1':
             count += 1
 
-        if b == '0' or i == len(row)-1:
+        if b == '0' or i == len(column)-1:
             if count in numbers:
                 numbers.remove(count)
                 count = 0
@@ -17,31 +19,39 @@ def valid_row(row, numbers):
 
 def getActions(row, n):
     s, e = n
+
     actions = []
     if s-1 >= 0:
-        if row[s-1] == 0:
+        if row[s-1] == '0':
+
             actions.append('L')
+
     if e+1 < len(row):
-        if row[e+1] == 0:
+        if row[e+1] == '0':
             actions.append('R')
     return actions
 
 
 def getPosition(row, numbers):
     result = []
-    for i in range(len(row)):
+
+    s = -1
+    e = -1
+    i = 0
+    while i < len(row):
         b = row[i]
         if b == '1':
-            if row[i-1] != '1':
-                s = i
-            count += 1
+            s = i
+            i = numbers[0] + i
+            e = i - 1
+            numbers.remove(numbers[0])
+            result.append([s, e])
+        i += 1
+    return result
 
-        if b == '0' or i == len(row)-1:
-            e = i
-        result.append([s, e])
 
+row = '011101'
+numbers = [3, 1]
 
-row = '0101'
-numbers = [1, 1]
-res = valid_row('0101', [1, 1])
-print(res)
+result = getPosition(row, numbers)
+print(result)
--- testCsp.py
+++ testCsp.py
@@ -1,21 +1,25 @@
 import unittest
 
-import Matrix
+import CSP
 
 
 class TestAddNumbers(unittest.TestCase):
 
-    def test_add_positive_numbers(self):
-        result = add_numbers(3, 5)
-        self.assertEqual(result, 8)
+    def test_valid_row(self):
+        result = CSP.valid_row('0101', [1, 1])
+        self.assertTrue(result)
+        result1 = CSP.valid_row('011101', [3, 1])
+        self.assertTrue(result1)
 
-    def test_add_negative_numbers(self):
-        result = add_numbers(-3, -5)
-        self.assertEqual(result, -8)
+    def test_get_positions(self):
+        result = CSP.getPosition('0101', [1, 1])
+        self.assertEqual(result, [[1, 1], [3, 3]])
+        result1 = CSP.getPosition('011101', [3, 1])
+        self.assertEqual(result1, [[1, 3], [5, 5]])
 
-    def test_add_mixed_numbers(self):
-        result = add_numbers(2, -4)
-        self.assertEqual(result, -2)
+    def test_getActions(self):
+        result = CSP.getActions('0101', [1, 1])
+        self.assertEqual(result, ['L', 'R'])
 
 
 if __name__ == '__main__':
